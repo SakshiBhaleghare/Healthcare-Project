91bee3c16797409ecc2e9b2de9316513a7df01cb
SakshiBhaleghare <bhalegharesakshi@gmail.com>
2025-06-24 21:03:43 +0530

    Add files via upload

---
+++ src/main/java/com/Service/UserService.java
@@ -0,0 +1,29 @@
+package com.Service;
+
+import java.util.List;
+
+import com.DTO.ResponseDTO;
+import com.Entity.Login;
+import com.Entity.User;
+
+import net.sf.jasperreports.engine.JRException;
+
+public interface UserService {
+
+//	public ResponseDTO userRegisterInService(User user);
+
+	public ResponseDTO userRegisterInService(Login login);
+
+	public boolean isEmailRegistered(String email);
+
+	public String createPasswordResetToken(String email);
+
+	public User updateUserByEmail(String email, User user);
+
+	public void deleteUserByEmail(String email);
+
+	public void generateUserReportInService(String format) throws JRException;
+
+	public void generatePatientReportInservice(String format) throws JRException;
+
+}
